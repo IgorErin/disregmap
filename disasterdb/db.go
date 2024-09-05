46eb4a496ebff1ce2983e373aff2630cb060622e
IgorErin <erin.igore@yandex.ru>
2024-09-05 23:37:31 +0300

    wip

--- disasterdb/db.go
+++ disasterdb/db.go
@@ -10,12 +10,11 @@ import (
 type Disaster struct {
 	Id       int `db:"id"`
 	RegionId int `db:"region_id"`
-	// TODO(add fields)
-	// TypeInfoId     int    `db: type_info_id`
-	// JScore         int    `db: j_score`
-	// Source         string `db: source`
-	// Description    string `db: description`
-	// DetailedSource string `db: detailed_source`
+	TypeInfoId     int    `db:"type_info_id"`
+	// JScore         int    `db:"j_score"`
+	// Source         string `db:"source"`
+	// Description    string `db:"description"`
+	// DetailedSource string `db:"detailed_source"`
 
 	YearStart  int `db:"year_start"`
 	StartDay   int `db:"start_day"`
@@ -79,4 +78,53 @@ func QueryRegion(ctx context.Context, db *sqlx.DB) []Region {
 	}
 
 	return res
+}
+
+type Date struct {
+	Day int 
+	Moth int 
+	Year int
+}
+
+type Action struct {
+	Region string
+	TypeInfo string
+
+	StartDate Date  
+	EndDate Date
+}
+
+func Retriev(ctx context.Context, db *sqlx.DB) []Action {
+	disasters := QueryDisaster(ctx, db)
+
+	typeInfo := QueryTypeInfo(ctx, db)
+	typeInfoMap := func () map[int]string {
+		m := make(map[int]string, len(typeInfo))
+		for _, info := range typeInfo {
+			m[info.Id] = info.Name
+		}
+		return m
+	}()
+
+	region := QueryRegion(ctx, db)
+	regionMap := func () map[int]string {
+		m := make(map[int]string, len(region))
+		for _, r := range region {
+			m[r.Id] = r.Name
+		}
+		return m
+	}()
+
+	results := make([]Action, 0, len(disasters))
+	for _, d := range disasters {
+		results = append(results, Action{ 
+			Region: regionMap[d.RegionId],
+			TypeInfo: typeInfoMap[d.TypeInfoId],
+
+			StartDate: Date {Day: d.StartDay, Moth: d.StartMonth, Year: d.YearStart },
+			EndDate: Date {Day: d.EndDay, Moth: d.EndMonth, Year: d.YearEnd },
+		})
+	}
+
+	return results
 }
